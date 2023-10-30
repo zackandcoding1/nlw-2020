e762b47ee06c3b28ff468357f98ea5c8c30939b2
Isaac Gomes <isaacnbgomes@gmail.com>
2023-10-30 12:41:09 -0300

    first commit

---
+++ src/database/fake_data.js
@@ -0,0 +1,37 @@
+// Dados
+const proffys = [
+    { 
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
+        whatsapp: "9138593489", 
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject: "Química", 
+        cost: "20",
+        weekday: [0], 
+        time_from: [720], 
+        time_to: [1220]
+    },
+    {
+        name: "Mayk Brito",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp: "89987654534",
+        bio: "Estusiasta das melhores tecnologias de química avançada.",
+        subject: "Química",
+        cost: "20",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220]
+    },
+    { 
+        name: "Ednaldo Pereira", 
+        avatar: "https://pbs.twimg.com/profile_images/1531810783117594626/ljZzGm4v_400x400.jpg", 
+        whatsapp: "9138593489", 
+        bio: "Você vale tudo.", 
+        subject: "História", 
+        cost: "60",
+        weekday: [1], 
+        time_from: [720], 
+        time_to: [1220] 
+    }
+
+]
